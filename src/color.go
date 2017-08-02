def32c95f2f291d47ee8c73f6db397f1164f38a5
LukeEuler <luke16times@gmail.com>
2017-08-02 16:25:05 +0800

    more color

--- src/color.go
+++ src/color.go
@@ -16,9 +16,37 @@ func green(content string) string {
 	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
 }
 
+func yellow(content string) string {
+	style := 1
+	front := 33
+	//back := 40
+	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
+}
+
 func blue(content string) string {
 	style := 1
 	front := 34
+	//back := 40
+	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
+}
+
+func magenta(content string) string {
+	style := 1
+	front := 35
+	//back := 40
+	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
+}
+
+func cyan(content string) string {
+	style := 1
+	front := 36
+	//back := 40
+	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
+}
+
+func lightGray(content string) string {
+	style := 1
+	front := 37
 	//back := 40
 	return fmt.Sprintf("%c[%d;%dm%s%c[0m ", 0x1B, style, front, content, 0x1B)
 }
